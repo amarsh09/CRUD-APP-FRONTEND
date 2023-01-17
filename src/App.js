9250b30f43ebc23c939d4e649f992a69366b07dc
amarsh <s.amarsh12@gmail.com>
2023-01-17 06:37:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import FormDetails from './Components/FormDetails';
+import UserDetails from './Components/UserDetails';
+
+function App() {
+  return (
+   <>
+   <FormDetails/>
+   <UserDetails/>
+   </>
+  );
+}
+
+export default App;
